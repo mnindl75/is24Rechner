350d268a321413ba58bc8bd98e05b4a1d021e0f0
mnindl75 <mnindl75@gmail.com>
2013-06-20 17:24:27 +0200

    erste version vom strompreisvergleichsrechner

--- Stromvergleichsrechner/js/stromvergleichsrechner.js
+++ Stromvergleichsrechner/js/stromvergleichsrechner.js
@@ -12,22 +12,29 @@ de.is24.newBiz.stromVergleichsRechner = de.is24.newBiz.stromVergleichsRechner ||
         "use strict";
 
         function initSlider() {
-            var slider = de.is24.slider.initSlider("#verbrauchs-slider", 1, 8, 1, "#personen-value", 1),
-                $verbrauch = $('input#verbrauch');
+            var slider = de.is24.slider.initSlider("#strom-vergleichs-rechner #verbrauchs-slider", 1, 8, 1, "#strom-vergleichs-rechner #personen-value", 1),
+                $verbrauch = $('#strom-vergleichs-rechner input#verbrauch'),
+                VERBRAUCHS_MAP = [0, 2000, 3500, 4250, 5000, 5750, 6500, 7250, 8000],
+                STROM_ANBIETER_SUCHE_URL = "http://www.immobilienscout24.de/umzug/umzugsunternehmen/stromanbieter.html?"
 
-            function  setVisualAndHiddenInput() {
+            function  setVisualAndVerbrauchAndUrLParams() {
                 var value = slider.value,
                     i = 0;
-                $verbrauch.val(value);
-                $('.helper-visual').removeClass("active");
+                $verbrauch.val(VERBRAUCHS_MAP[value] + " kWh");
+                $('#strom-vergleichs-rechner .btn-action')
+                    .attr("href", STROM_ANBIETER_SUCHE_URL + "zip=" + $('#zip').val() + "&usg=" + VERBRAUCHS_MAP[value])
+                $('#strom-vergleichs-rechner .visual').removeClass("active");
                 for (i; i <= value; i = i + 1 ) {
-                    $('#helper-visual-' + i).addClass("active");
+                    $('#strom-vergleichs-rechner #visual-' + i).addClass("active");
                 }
             }
             slider.changeHandler = function () {
-                setVisualAndHiddenInput();
+                setVisualAndVerbrauchAndUrLParams();
             };
-            setVisualAndHiddenInput();
+            $('#zip').change(function() {
+                setVisualAndVerbrauchAndUrLParams();
+            })
+            setVisualAndVerbrauchAndUrLParams();
         }
 
 
